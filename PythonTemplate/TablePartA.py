65858835cbd80cea343a71dded126fc188f90f12
Nick Messina <nicknmessina@gmail.com>
2024-03-27 15:51:13 -0500

    Part A done

--- PythonTemplate/TablePartA.py
+++ PythonTemplate/TablePartA.py
@@ -1,1 +1,12 @@
 import happybase as hb
+
+# Establish a connection to HBase
+connection = hb.Connection("localhost")
+
+# Create the 'powers' table with 'personal', 'professional', and 'custom' column families
+connection.create_table(
+    "powers", {"personal": dict(), "professional": dict(), "custom": dict()}
+)
+
+# Create the 'food' table with 'nutrition' and 'taste' column families
+connection.create_table("food", {"nutrition": dict(), "taste": dict()})
