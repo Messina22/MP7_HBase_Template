099e392751141d0808f09d43296b64f1537543b9
Nick Messina <nicknmessina@gmail.com>
2024-03-27 17:57:00 -0500

    Completed part F

--- PythonTemplate/TablePartF.py
+++ PythonTemplate/TablePartF.py
@@ -3,14 +3,21 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-#1. update the data in a particular cell using the put() method
-#2. retrieve all versions of all columns, with the most recent version coming first
-                                                                                     
-row = ???
-for ???, ??? in row.items():
+# 1. update the data in a particular cell using the put() method
+# 2. retrieve all versions of all columns, with the most recent version coming first
 
-    cells = ???
-    for ???, ??? in cells:
-        print("row: {}, column family:qualifier: {}, value: {}, timestamp: {}".format(???, ???, ???, ???))
+connection = hb.Connection("localhost")
 
+table = connection.table("powers")
 
+table.put(b"row7", {b"custom:color": b"purple"})
+
+row = table.row(b"row7")
+for key, value in row.items():
+    cells = table.cells(b"row7", key, include_timestamp=True, versions=2)
+    for name, time in cells:
+        print(
+            "row: {}, column family:qualifier: {}, value: {}, timestamp: {}".format(
+                b"row7", key, name, time
+            )
+        )
