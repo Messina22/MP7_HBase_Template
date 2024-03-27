07bddb47d887152a4d93a6b9cf11ea2c88253bb6
Nick Messina <nicknmessina@gmail.com>
2024-03-27 18:10:42 -0500

    Finished part G

--- PythonTemplate/TablePartG.py
+++ PythonTemplate/TablePartG.py
@@ -3,14 +3,19 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-color = ???
-name = ???
-power = ???
+connection = hb.Connection("localhost")
+table = connection.table("powers")
 
-color1 = ???
-name1 = ???
-power1 = ???
+rows = [row for row in table.scan()]
 
-print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+for key, data in rows:
+    color = data[b"custom:color"]
+    name = data[b"professional:name"]
+    power = data[b"personal:power"]
+    for key1, data1 in rows:
+        color1 = data1[b"custom:color"]
+        name1 = data1[b"professional:name"]
+        power1 = data1[b"personal:power"]
 
-
+        if color == color1 and name != name1:
+            print("{}, {}, {}, {}, {}".format(name, power, name1, power1, color))
