b412b3268b21ce838b912dfb06c965c8984dfa1b
Nick Messina <nicknmessina@gmail.com>
2024-03-27 16:30:13 -0500

    Part C and Part D completed

--- PythonTemplate/TablePartC.py
+++ PythonTemplate/TablePartC.py
@@ -1,3 +1,26 @@
 import happybase as hb
 import csv
 
+# Establish a connection to HBase
+connection = hb.Connection("localhost")
+
+# Get a reference to the 'powers' table
+table = connection.table("powers")
+
+# Open the CSV file
+with open("input.csv", "r") as file:
+    reader = csv.reader(file)
+
+    # For each row in the CSV file
+    for row in reader:
+        # Insert the data into the 'powers' table
+        table.put(
+            str(row[0]).encode(),
+            {
+                b"personal:hero": str(row[1]).encode(),
+                b"personal:power": str(row[2]).encode(),
+                b"professional:name": str(row[3]).encode(),
+                b"professional:xp": str(row[4]).encode(),
+                b"custom:color": str(row[5]).encode(),
+            },
+        )
--- PythonTemplate/TablePartD.py
+++ PythonTemplate/TablePartD.py
@@ -3,20 +3,56 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-hero = ???
-power = ???
-name = ???
-xp = ???
-color = ???
+connection = hb.Connection("localhost")
+table = connection.table("powers")
 
-print('hero: {}, power: {}, name: {}, xp: {}, color: {}'.format(hero, power, name, xp, color))
+# rows = table.scan()
+# for key, data in rows:
+#     print(key, data)
 
-hero = ???
-color = ???
+data = table.row(
+    b"row1",
+    columns=[
+        "personal:hero",
+        "personal:power",
+        "professional:name",
+        "professional:xp",
+        "custom:color",
+    ],
+)
+hero = data[b"personal:hero"]
+power = data[b"personal:power"]
+name = data[b"professional:name"]
+xp = data[b"professional:xp"]
+color = data[b"custom:color"]
 
-print('hero: {}, color: {}'.format(hero, color))
+print(
+    "hero: {}, power: {}, name: {}, xp: {}, color: {}".format(
+        hero, power, name, xp, color
+    )
+)
 
-hero = ???
-name = ???
-color = ???
-print('hero: {}, name: {}, color: {}'.format(hero, name, color))
+data = table.row(
+    b"row19",
+    columns=[
+        "personal:hero",
+        "custom:color",
+    ],
+)
+hero = data[b"personal:hero"]
+color = data[b"custom:color"]
+
+print("hero: {}, color: {}".format(hero, color))
+
+data = table.row(
+    b"row1",
+    columns=[
+        "personal:hero",
+        "professional:name",
+        "custom:color",
+    ],
+)
+hero = data[b"personal:hero"]
+name = data[b"professional:name"]
+color = data[b"custom:color"]
+print("hero: {}, name: {}, color: {}".format(hero, name, color))
